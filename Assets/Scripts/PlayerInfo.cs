7302ad0addd4102085c280593bcfd7a0d7cc4dc8
Charlotte D <charlotte.dalby@midlandheart.org.uk>
2023-05-12 15:01:14 +0100

    Adding in new card asset images and changing defeat screen background

---
+++ Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerInfo
+{
+
+    public static int health;
+    public static List<Card> cards;
+}
